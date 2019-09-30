714fa5eb44604c14512a03ac52843fa317bd516f
LmintSin <minh-thong.lu@cyberskill.tech>
2019-09-30 13:15:51 +0700

    Enable commit build folder for auto deploy

---
+++ build/precache-manifest.be7a768b6c85cc3f9d58dac24d3a5205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6877f5fe9c379a1a58303717b50975d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7b5ac2b1e51d32f6748",
+    "url": "/static/css/2.388b96e4.chunk.css"
+  },
+  {
+    "revision": "e2d6afe107b961af6e10",
+    "url": "/static/css/main.b206d239.chunk.css"
+  },
+  {
+    "revision": "e7b5ac2b1e51d32f6748",
+    "url": "/static/js/2.9ded0326.chunk.js"
+  },
+  {
+    "revision": "e2d6afe107b961af6e10",
+    "url": "/static/js/main.b6e7005e.chunk.js"
+  },
+  {
+    "revision": "e72921bdbf90c087d29f",
+    "url": "/static/js/runtime-main.4f337f23.js"
+  }
+]);
